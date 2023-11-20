adc8792179d0a639b99e6abdc3023bd11801737f
ElianaCepeda <eli.cepedagonzalez@gmail.com>
2023-11-20 15:51:11 -0500

    Agregando comentario de prueba

--- src/main/java/com/example/hola/RentalCarApplication.java
+++ src/main/java/com/example/hola/RentalCarApplication.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class RentalCarApplication extends Application {
+    //
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(RentalCarApplication.class.getResource("logInView.fxml"));
